72597c1ddc22c8a239b66dc9e82e2bf933edee04
Cat Kutay <cat.kutay@gmail.com>
2016-03-04 09:41:15 +1100

    Added files via upload

---
+++ otherfiles/NetworkCharacter.cs
@@ -0,0 +1,65 @@
+﻿/*using UnityEngine;
+using System.Collections;
+
+
+public class NetworkCharatcer : Photon.MonoBehaviour {
+
+
+    Vector3 realPosition = Vector3.zero;
+    Quaternion realRotation = Quaternion.identity;
+    float lastUpdateTime;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        // PhotonView photonView = GetComponent<PhotonView>();
+        if (photonView.isMine)
+        {
+            // do nothing
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, realPosition, 0.1f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, 0.1f);
+        }
+		/*if (transform.position != realPosition) {
+			anim.SetBool ("Running", true);		
+		}
+		else
+		{
+			anim.SetBool ("Running", false);	
+		}
+	}
+
+	// send/recieve information from other clients
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+			stream.SendNext (anim.GetFloat("Forward"));
+			//stream.SendNext (anim.GetBool ("m_IsGrounded"));
+			//stream.SendNext (anim.GetFloat("Jump"));
+			stream.SendNext (anim.GetFloat("Turn"));  //m_IsGrounded
+
+        }
+        else
+        {
+            realPosition = (Vector3)stream.ReceiveNext();
+            realRotation = (Quaternion)stream.ReceiveNext();
+			anim.SetFloat ("Forward", (float)stream.ReceiveNext ());
+			//anim.SetBool ("m_IsGrounded", (bool)stream.ReceiveNext ());
+			//anim.SetFloat ("Jump", (float)stream.ReceiveNext ());
+			anim.SetFloat ("Turn", (float)stream.ReceiveNext ());
+        }
+    }
+}
+*/
---
+++ otherfiles/NetworkController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.Networking;
+using SimpleJSON;
+using System.Linq;
+
+
+public class NetworkController : NetworkManager
+{
+	public short playerPrefabIndex;
+
+	public override void OnStartServer()
+	{
+		NetworkServer.RegisterHandler(MsgTypes.PlayerPrefab, OnResponsePrefab);
+		base.OnStartServer();
+	}
+
+	public override void OnClientConnect(NetworkConnection conn)
+	{
+		client.RegisterHandler(MsgTypes.PlayerPrefab, OnRequestPrefab);
+		base.OnClientConnect(conn);
+	}
+
+	private void OnRequestPrefab(NetworkMessage netMsg)
+	{
+		MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
+		msg.controllerID = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>().controllerID;
+		msg.prefabIndex = playerPrefabIndex;
+		client.Send(MsgTypes.PlayerPrefab, msg);
+	}
+
+	private void OnResponsePrefab(NetworkMessage netMsg)
+	{
+		MsgTypes.PlayerPrefabMsg msg = netMsg.ReadMessage<MsgTypes.PlayerPrefabMsg>();  
+		playerPrefab = spawnPrefabs[msg.prefabIndex];
+		base.OnServerAddPlayer(netMsg.conn, msg.controllerID);
+		Debug.Log(playerPrefab.name + " spawned!");
+	}
+
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+	{
+		MsgTypes.PlayerPrefabMsg msg = new MsgTypes.PlayerPrefabMsg();
+		msg.controllerID = playerControllerId;
+		NetworkServer.SendToClient(conn.connectionId, MsgTypes.PlayerPrefab, msg);
+	}
+}
